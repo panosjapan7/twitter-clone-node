ad9b0652e195a135594d6d71cf72b298a01125c5
panosjapan7 <panosjapan@gmail.com>
2022-02-22 08:56:17 +0100

    twitter clone nr.5 attempt

---
+++ app.js
@@ -0,0 +1,5 @@
+// Fifth attempt at creating a Twitter clone for the Backend 1 exercise
+// I tried doing it with .ejs files but had problem injecting code to HTML elements; it's much easier with PUG.
+// I tried using the strategies and code we use in the lektioner but haven't understood much.
+// I'll be using internet tutorials to try and solve this puzzle.
+
