9ca4f88d9c7adbb83755bce36f325a7ffb2f5e49
panosjapan7 <panosjapan@gmail.com>
2022-02-22 09:27:12 +0100

    set up pug, added a app.get() that renders home.pug

--- app.js
+++ app.js
@@ -16,3 +16,17 @@ const server = app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 })
 // Creates out server instance. We tell it which port to listen to and then what code to run once it starts listening.
+
+app.set("view engine", "pug");
+// Tells our server which template engine we're going to use to display web pages.
+app.set("views", "views");
+// Tells the server that when it needs a view page (a template), go to the folder called "views" to look for the file.
+
+app.get("/", (req, res, next) => {
+    
+    res.status(200).render("home.pug");
+    //200 HTTP response is for 'success'; it means all is okay.
+
+});
+// When someone accesses the server we use with a GET request and the path is the root of the website ("/"), execute the code that's in the arrow function.
+// req(the request that's incoming to this path "/"), res(response -  the data we send back to the user), next(handles any middleware)
