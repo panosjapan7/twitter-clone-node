cb31f51c78841542ac919dfb0420814f2d319e4e
panosjapan7 <panosjapan@gmail.com>
2022-02-22 09:07:12 +0100

    declared Express dependency and initialized it, created a server

--- app.js
+++ app.js
@@ -3,3 +3,16 @@
 // I tried using the strategies and code we use in the lektioner but haven't understood much.
 // I'll be using internet tutorials to try and solve this puzzle.
 
+const express = require("express");
+// Declares Express dependency and tells Node that we're using Express with this file.
+
+const app = express();
+// Initializes Express by making app variable an instance of the Express object
+
+const PORT = 7000;
+// Creates the port that the server of this app will run on.
+
+const server = app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+})
+// Creates out server instance. We tell it which port to listen to and then what code to run once it starts listening.
