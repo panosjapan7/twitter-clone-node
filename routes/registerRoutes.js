2a3db1f233625be6d8ba31b09ff32f7f5106f9eb
panosjapan7 <panosjapan@gmail.com>
2022-02-22 15:10:30 +0100

    added body-parser, console.log req.body - it works

--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -4,12 +4,18 @@ const app = express();
 // Initializes Express by making app variable an instance of the Express object
 const router = express.Router();
 // creates const "router" which allows the file to use the Router function that comes with Express framework.
-
+const bodyParser = require("body-parser");
+// Uses body-parser package
+// When we submit a form, the data that's sent to the server is sent in the request body. So we need to get it from the body.
 
 app.set("view engine", "pug");
 // Tells our server which template engine we're going to use to display web pages.
 app.set("views", "views");
 // Tells the server that when it needs a view page (a template), go to the folder called "views" to look for the file.
+
+app.use(bodyParser.urlencoded({ extended: false }));
+// Tells the app to use body-parser; "extended: false" is a setting and it means that the body 
+// will be able to contain only key value pairs made up of strings or arrays (not all types of data).
 
 router.get("/", (req, res, next) => {
 // Configures the router we created (const router = express.Router();) 
@@ -22,6 +28,16 @@ router.get("/", (req, res, next) => {
     //200 HTTP response is for 'success'; it means all is okay.
 
 });
+
+router.post("/", (req, res, next) => {
+//Because in the form in register.pug we use a POST method to submit the data, we use router.post (instead router.get)
+
+        console.log(req.body);
+
+        res.status(200).render("register.pug");
+        //200 HTTP response is for 'success'; it means all is okay.
+    
+    });
 
 module.exports = router;
 // Exports the router so we can execute the code that's in router.get in other pages.
