d69f82a0c75c13886df7bb2c04958e5480db77f8
panosjapan7 <panosjapan@gmail.com>
2022-02-24 09:13:47 +0100

    created the user Schema for when a user registers an account

---
+++ schemas/UserSchema.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+// Adds a connection to our MongoDB db
+
+const Schema = mongoose.Schema;
+// We declare that mongoose.Schema is a type of Schema
+// We type uppercase 'S' because this is going to be a type.
+
+const UserSchema = new  Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    firstName: {
+        type: String,
+        trim: true,
+    },
+    lastName: {
+        type: String,
+        trim: true,
+    },
+    profilePic: {
+        type: String,
+        default: "/images/profilePic.png",
+    }
+});
+// Creates the schema for our user collection. In the parentheses we pass in the options (the columns and data types);
+
+var User = mongoose.model("User", UserSchema);
+module.exports = User;
+// Exports the schema. The model() function is how you export a model. 
+// It allows all connections to the "mongoose = require("mongoose");" db to have access to this schema by accessing the "User" property.
