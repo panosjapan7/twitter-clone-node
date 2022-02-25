2017b036ffc51fbd4f2c685062caf0ba23d74284
panosjapan7 <panosjapan@gmail.com>
2022-02-25 11:40:19 +0100

    I save the value typed into the textbox

--- public/js/common.js
+++ public/js/common.js
@@ -1,6 +1,13 @@
 // common.js will include all the shared js code between our site will be
 
-$(document).ready(() => {
-    alert("Hell Panos")
+$("#postTextarea").keyup((event) => {
+// this function will fire off when the user selects the textarea and presses a key on the keyboard.
+    var textbox = $(event.target);
+    // Creates a javascript object by using the event.target. The target event property returns the element that triggered the event. 
+    
+    var value = textbox.val().trim();
+    // .val() is a built-in function that gives you the value of this element (in our case is the text inside the textbox)
+    // I used trim() so that users can't create empty posts.
+    console.log(value);
 })
 
