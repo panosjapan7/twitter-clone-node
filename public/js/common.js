b6a5c4341dfa5b11b592fb3a7a36f383f5dbc1e6
panosjapan7 <panosjapan@gmail.com>
2022-02-25 11:47:51 +0100

    disable button if textbox is empty or consists of only spaces

--- public/js/common.js
+++ public/js/common.js
@@ -8,6 +8,22 @@ $("#postTextarea").keyup((event) => {
     var value = textbox.val().trim();
     // .val() is a built-in function that gives you the value of this element (in our case is the text inside the textbox)
     // I used trim() so that users can't create empty posts.
-    console.log(value);
+    
+    var submitButton = $("#submitPostButton");
+    // Stores the button element as a variable called "submitButton"
+
+    if(submitButton.length == 0) return alert("No submit button found.");
+    // this checks if an html element with the id "submitPostButton" exists. 
+    // If there isn't (if length == 0) it ends the execution of the function here (because I return the alert).
+
+    if(value == "") {
+        submitButton.prop("disabled", true);
+        return;
+        // Checks if the value in the textbox is empty, and sets the submit button to disabled if it is.
+    }
+
+    submitButton.prop("disabled", false);
+    return;
+    // Checks if the value in the textbox is empty, and sets the submit button to enabled if it's not empty.
 })
 
